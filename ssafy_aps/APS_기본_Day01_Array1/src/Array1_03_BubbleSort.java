0d7acb863172b3f1823d15b4749d8077857c2d75
junghoon1039 <junghoon1039@gmail.com>
2023-08-08 13:10:13 +0900

    sort

--- ssafy_aps/APS_기본_Day01_Array1/src/Array1_03_BubbleSort.java
+++ ssafy_aps/APS_기본_Day01_Array1/src/Array1_03_BubbleSort.java
@@ -4,8 +4,8 @@ public class Array1_03_BubbleSort {
     public static void main(String[] args) {
         int[] nums = { 24, 99999, 99, 31, 213124, 7, 35 };
         int N = nums.length;
-        for (int i = N - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
                 if (nums[j] > nums[j + 1]) {
                     int num = nums[j];
                     nums[j] = nums[j + 1];
